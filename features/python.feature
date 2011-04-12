53df4f04ead770de2bc2c9a1ea7ff38bc4091167
Jan Brauer <jan@jimdo.com>
2011-04-12 23:39:44 +0200

    Test that requirements are being installed.

--- features/python.feature
+++ features/python.feature
@@ -4,7 +4,13 @@ Feature: Python
     When I do a vagrant provision
     Then the Python package "virtualenv" should be installed
 
-  @wip
   Scenario: A virtualenv for deployment should be created
     When I do a vagrant provision
     Then a virtualenv should be created at "/home/vagrant/virtualenv"
+
+  @wip
+  Scenario: All requirements should be installed
+    When I do a vagrant provision
+    Then these Python packages should be installed in "/home/vagrant/virtualenv":
+      |package|version|
+      |django |1.2.3  |
--- features/step_definitions/python_steps.rb
+++ features/step_definitions/python_steps.rb
@@ -19,3 +19,17 @@ Then /^a virtualenv should be created at "([^"]*)"$/ do |path|
     Then it should have exitcode 0
   }
 end
+
+Then /^these Python packages should be installed in "([^"]*)":$/ do |path, table|
+  When "I ssh to vagrantbox \"default\" with the following credentials:", table(%{
+    | username | password|
+    | vagrant  | vagrant | 
+  })
+  steps %Q{
+    When I run "#{path}/bin/pip freeze" 
+  }
+  packages = table.hashes
+  packages.each do |pkg|
+    @output.downcase.should include "#{pkg['package']}==#{pkg['version']}"
+  end
+end
